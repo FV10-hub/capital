52e5953dfec44e55aaa84624c904efa1b7569e5d
Fernando Vazquez <fernandorafa@live.com>
2025-06-29 13:13:52 -0300

    primer commit

---
+++ src/test/java/py/com/capital/CapitaCreditos/CapitaCreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.capital.CapitaCreditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitaCreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
